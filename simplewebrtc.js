271879000226384645f1738a1847730037ba9c79
Réda Housni Alaoui <reda-alaoui@users.noreply.github.com>
2014-02-05 00:29:59 +0100

    Allow to change socketio client default conf

--- simplewebrtc.js
+++ simplewebrtc.js
@@ -52,7 +52,7 @@ function SimpleWebRTC(opts) {
     WildEmitter.call(this);
 
     // our socket.io connection
-    connection = this.connection = io.connect(this.config.url);
+    connection = this.connection = io.connect(this.config.url, this.config);
 
     connection.on('connect', function () {
         self.emit('connectionReady', connection.socket.sessionid);

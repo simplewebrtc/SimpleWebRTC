885bf9afd7deb208ad7596501aec2fb60589675f
Philipp Hancke <fippo@andyet.net>
2015-09-23 20:56:29 -0700

    argh

--- audio.js
+++ audio.js
@@ -169,7 +169,7 @@ if (room) {
 
 function GUM() {
     webrtc = new SimpleWebRTC({
-        //url: 'https://api.talky.io', // this will only work from simplewebrtc.com, please use the default sandbox otherwise
+        url: 'https://api.talky.io', // this will only work from simplewebrtc.com, please use the default sandbox otherwise
         // we don't do video
         localVideoEl: '',
         remoteVideosEl: '',

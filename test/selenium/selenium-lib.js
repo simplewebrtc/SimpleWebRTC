255fe3e4c82ba60ac1f78789ebdbf29fa8a1bd3a
Philipp Hancke <fippo@andyet.net>
2015-09-27 17:33:23 -0700

    close chrome; fix jshint nit

--- test/selenium/selenium-lib.js
+++ test/selenium/selenium-lib.js
@@ -9,23 +9,23 @@ function buildDriver(browser) {
     var profile = new firefox.Profile();
     profile.setPreference('media.navigator.streams.fake', true);
     var firefoxOptions = new firefox.Options()
-        .setBinary('node_modules/.bin/start-firefox')
+        .setBinary('browsers/bin/firefox-stable')
         .setProfile(profile);
 
     // Chrome options.
     // http://selenium.googlecode.com/git/docs/api/javascript/module_selenium-webdriver_chrome_class_Options.html#addArguments
     var chromeOptions = new chrome.Options()
-        .setChromeBinaryPath('node_modules/.bin/start-chrome')
+        .setChromeBinaryPath('browsers/bin/chrome-stable')
         .addArguments('allow-file-access-from-files')
         .addArguments('use-fake-device-for-media-stream')
         .addArguments('use-fake-ui-for-media-stream');
         // use-file-for-fake-audio-capture -- see https://code.google.com/p/chromium/issues/detail?id=421054
 
-  return new webdriver.Builder()
-      .forBrowser(browser || process.env.BROWSER || 'firefox')
-      .setFirefoxOptions(firefoxOptions)
-      .setChromeOptions(chromeOptions)
-      .build();
+    return new webdriver.Builder()
+        .forBrowser(browser || process.env.BROWSER || 'firefox')
+        .setFirefoxOptions(firefoxOptions)
+        .setChromeOptions(chromeOptions)
+        .build();
 }
 
 module.exports = {

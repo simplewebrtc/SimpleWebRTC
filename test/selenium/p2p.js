860c7847afc9260f5f6ddb47dc9d50c096321bea
Philipp Hancke <fippo@appear.in>
2017-06-21 21:34:49 +0200

    test: update tests to use webdriver lib from testbed
    which also pulls selenium + friends from that package.
    Also installs chrome/firefox via travis-multirunner and uses
    those versions.

--- test/selenium/p2p.js
+++ test/selenium/p2p.js
@@ -2,39 +2,44 @@
 var test = require('tape');
 
 // https://code.google.com/p/selenium/wiki/WebDriverJs
-var seleniumHelpers = require('./selenium-lib');
+var seleniumHelpers = require('webrtc-testbed/webdriver');
 var webdriver = require('selenium-webdriver');
 
 function doJoin(driver, room) {
     return driver.get('file://' + process.cwd() + '/test/index.html?' + room);
 }
 
+function iceConnected(driver) {
+    return driver.wait(() => {
+        return driver.executeScript(function () {
+            return window.webrtc && window.webrtc.getPeers().length === 1 &&
+                (window.webrtc.getPeers()[0].pc.iceConnectionState === 'connected' ||
+                 window.webrtc.getPeers()[0].pc.iceConnectionState === 'completed');
+        });
+    }, 30 * 1000)
+}
+
 function testP2P(browserA, browserB, t) {
     var room = 'testing_' + Math.floor(Math.random() * 100000);
 
-    var userA = seleniumHelpers.buildDriver(browserA);
-    doJoin(userA, room);
-
-    var userB = seleniumHelpers.buildDriver(browserB);
-    doJoin(userB, room);
-
-    userA.wait(function () {
-        return userA.executeScript(function () {
-            return window.webrtc && window.webrtc.getPeers().length === 1 && window.webrtc.getPeers()[0].pc.iceConnectionState === 'connected';
-        });
-    }, 30 * 1000)
-    .then(function () {
+    var userA = seleniumHelpers.buildDriver(browserA, {bver: process.env.BVER});
+    var userB = seleniumHelpers.buildDriver(browserB, {bver: process.env.BVER});
+    return Promise.all([doJoin(userA, room), doJoin(userB, room)])
+    .then(() => {
+        t.pass('joined room');
+        return Promise.all([iceConnected(userA), iceConnected(userB)])
+    })
+    .then(() => {
         t.pass('P2P connected');
-        userA.quit();
-        userB.quit().then(function () {
-            t.end();
-        });
+        return Promise.all([userA.quit(), userB.quit()])
+    })
+    .then(() => { 
+        t.end();
     })
     .then(null, function (err) {
-        t.fail(err);
-        userA.quit();
-        userB.quit();
-    })
+        return Promise.all([userA.quit(), userB.quit()])
+            .then(() => t.fail(err));
+    });
 }
 
 test('P2P, Chrome-Chrome', function (t) {

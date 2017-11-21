3c34299d669e8468a2c397966416764c884088b0
Philipp Hancke <fippo@appear.in>
2017-11-21 13:00:30 +0100

    test: add more tests
    adds the "all videos have enough data" test from
      https://github.com/fippo/testbed/blob/master/simplewebrtc.js

--- test/selenium/p2p.js
+++ test/selenium/p2p.js
@@ -19,25 +19,82 @@ function iceConnected(driver) {
     }, 30 * 1000)
 }
 
-function testP2P(browserA, browserB, t) {
-    var room = 'testing_' + Math.floor(Math.random() * 100000);
+const TIMEOUT = 30000;
+function waitNPeerConnectionsExist(driver, n) {
+    return driver.wait(function() {
+        return driver.executeScript(function(n) {
+            return webrtc.getPeers().length === n;
+        }, n);
+    }, TIMEOUT);
+}
 
-    var userA = seleniumHelpers.buildDriver(browserA, {bver: process.env.BVER});
-    var userB = seleniumHelpers.buildDriver(browserB, {bver: process.env.BVER});
-    return Promise.all([doJoin(userA, room), doJoin(userB, room)])
+function waitAllPeerConnectionsConnected(driver) {
+    return driver.wait(function() {
+        return driver.executeScript(function() {
+            var peers = webrtc.getPeers();
+            var states = [];
+            peers.forEach(function(peer) {
+                states.push(peer.pc.iceConnectionState);
+            });
+            return states.length === states.filter((s) => s === 'connected' || s === 'completed').length;
+        });
+    }, TIMEOUT);
+}
+
+function waitNVideosExist(driver, n) {
+    return driver.wait(function() {
+        return driver.executeScript(function(n) {
+            return document.querySelectorAll('video').length === n;
+        }, n);
+    }, TIMEOUT);
+}
+
+function waitAllVideosHaveEnoughData(driver) {
+    return driver.wait(function() {
+        return driver.executeScript(function() {
+            var videos = document.querySelectorAll('video');
+            var ready = 0;
+            for (var i = 0; i < videos.length; i++) {
+                if (videos[i].readyState >= videos[i].HAVE_ENOUGH_DATA) {
+                    ready++;
+                }
+            }
+            return ready === videos.length;
+        });
+    }, TIMEOUT);
+}
+
+function testP2P(browserA, browserB, t) {
+    const room = 'testing_' + Math.floor(Math.random() * 100000);
+
+    const driverA = seleniumHelpers.buildDriver(browserA, {bver: process.env.BVER});
+    const driverB = seleniumHelpers.buildDriver(browserB, {bver: process.env.BVER});
+    const drivers = [driverA, driverB];
+
+    return Promise.all(drivers.map(driver => doJoin(driver, room)))
     .then(() => {
         t.pass('joined room');
-        return Promise.all([iceConnected(userA), iceConnected(userB)])
+        return Promise.all(drivers.map(driver => waitNPeerConnectionsExist(driver, 1)));
+    })
+    .then(() => {
+        return Promise.all(drivers.map(driver => waitAllPeerConnectionsConnected(driver)));
     })
     .then(() => {
         t.pass('P2P connected');
-        return Promise.all([userA.quit(), userB.quit()])
+        return Promise.all(drivers.map(driver => waitNVideosExist(driver, 2)));
+    })
+    .then(() => {
+        return Promise.all(drivers.map(driver => waitAllVideosHaveEnoughData(driver)));
+    })
+    .then(() => {
+        t.pass('all videos have enough data');
+        return Promise.all(drivers.map(driver => driver.quit()));
     })
     .then(() => { 
         t.end();
     })
     .then(null, function (err) {
-        return Promise.all([userA.quit(), userB.quit()])
+        return Promise.all(drivers.map(driver => driver.quit()))
             .then(() => t.fail(err));
     });
 }
